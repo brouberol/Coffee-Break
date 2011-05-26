9a4be30ab3d99280988eaa31e3fb15b1904d6f5d
Balthazar Rouberol <balto@Balto-Latitude.(none)>
2011-05-26 15:34:28 +0200

    v0.1 : 10 minutes fixed

---
+++ CoffeeBreak.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python2.6
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+import os
+import math
+import gobject
+import appindicator
+import pynotify
+from time import *
+gtk.gdk.threads_init()
+
+
+BREAK_TIME = 60*10 # 10 minutes
+
+class CoffeeBreak:
+
+    def __init__(self):
+        dirPath = os.path
+        self.icon = gtk.status_icon_new_from_file(os.path.realpath(self.get_dir() + "Coffee_icon_small.png"))
+        self.icon.set_tooltip("You still have 10 minutes of coffee break.\n Enjoy them !")
+        self.icon.connect('activate',self.icon_click) # icon_click changes status 
+        self.icon.set_visible(True)
+
+        self.menu = gtk.Menu()
+        self.menuItem = gtk.ImageMenuItem(gtk.STOCK_QUIT)
+        self.menuItem.connect('activate', self.quit_cb, self.icon)
+        self.menu.append(self.menuItem)
+	
+	self.icon.connect('popup-menu', self.popup_menu_cb, self.menu)
+
+	self.status = "coffee"
+        self.deltaT = 10
+        self.start_working_time = time()
+
+    def popup_menu_cb(self, widget, button, time, data = None):
+
+        if button == 3:
+            if data:
+                data.show_all()
+                data.popup(None, None, gtk.status_icon_position_menu,3, time, self.icon)
+
+    def quit_cb(self, widget, data = None):
+        gtk.main_quit()
+
+    def get_dir(self):
+        return os.path.dirname(os.path.realpath(__file__)) + os.path.sep
+    
+    def calc_time(self, t, Break = None):
+        if Break == None:
+            minutes = math.floor(t/60)
+            if minutes > 1 :
+                return "{0} minutes".format(int(abs(minutes)))
+            else:
+                return "{0} minute".format(int(abs(minutes)))
+        else:
+            minutes = math.ceil((Break - t)/60)
+            if minutes > 1 :
+                return "{0} minutes".format(int(abs(minutes)))
+            else:
+                return "{0} minute".format(int(abs(minutes)))
+
+    def set_status(self, status):
+        if status == "coffee":
+            self.icon.set_from_file(self.get_dir() + "Coffee_icon_small.png")
+        else:
+            self.icon.set_from_file(self.get_dir() + "Coffee_icon_grey_small.png")
+        self.status = status
+       
+    def icon_click(self, dummy):
+        if self.status == "coffee":
+            self.set_status("working")
+	    self.icon.set_tooltip("Coffee break : procrastination fighter")
+        else:
+            self.set_status("coffee")
+ 	    self.start_working_time = time()
+ 
+    def update(self):
+        """This method is called everytime after deltaT seconds"""
+        delta = time() - self.start_working_time
+        if self.status == "working":
+            pass
+        else:
+            self.icon.set_tooltip("You still have {0} of coffee break.\n Enjoy them !".format(self.calc_time(delta, BREAK_TIME)))
+            if self.status == "coffee": 
+                if delta > BREAK_TIME:
+                    self.set_status("working")
+                    self.start_working_time = time()
+                    initCaps()
+                    path = self.get_dir() + "Coffee_icon.png"
+                    n = pynotify.Notification ("CoffeeBreak","Your coffee break is over. Back to work =)", path)
+                    n.show()            
+                    self.icon.set_tooltip("CoffeeBreak : procrastination killer")
+
+
+        source_id = gobject.timeout_add(self.deltaT*1000, self.update) #*1000 : valeur compensatoire de ralentissement
+
+    def main(self):
+        source_id = gobject.timeout_add(self.deltaT, self.update)
+        gtk.main()
+
+
+ 
+capabilities = {'actions':             False,
+                'body':                False,
+                'body-hyperlinks':     False,
+                'body-images':         False,
+                'body-markup':         False,
+                'icon-multi':          False,
+                'icon-static':         False,
+                'sound':               False,
+                'image/svg+xml':       False,
+                'private-synchronous': False,
+                'append':              False,
+                'private-icon-only':   False}
+
+def initCaps ():
+        caps = pynotify.get_server_caps ()
+        if caps is None:
+                print "Failed to receive server caps."
+                sys.exit (1)
+
+        for cap in caps:
+                capabilities[cap] = True
+
+
+#MAIN()
+if __name__ == "__main__":
+    app = CoffeeBreak()
+    app.main()
+
